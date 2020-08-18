efd3b79c5505d89a5b702f48c81aad6b02c4c69e
paul-writes-code <59710585+paul-writes-code@users.noreply.github.com>
2020-08-18 17:47:28 -0400

    jar file will launch a terminal to run the game

--- src/com/example/TextAdventure/Main.java
+++ src/com/example/TextAdventure/Main.java
@@ -1,8 +1,20 @@
 package com.example.TextAdventure;
 
+import java.awt.*;
+import java.io.Console;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        Console console = System.console();
+
+        // If there is no open console, then one is launched to run the game
+        if (console == null && !GraphicsEnvironment.isHeadless()) {
+            String fileName = Main.class.getProtectionDomain().getCodeSource().getLocation().toString().substring(6);
+            Runtime.getRuntime().exec(new String[]{"cmd","/c","start","cmd","/k","java -jar \"" + fileName + "\""});
+        }
+
         World.runGame();
     }
 }
